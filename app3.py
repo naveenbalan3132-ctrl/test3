bb1ff9b9c08c307e28b977779b2873ce115c104e
naveenbalan3132-ctrl <naveenbalan3132@gmail.com>
2025-11-28 23:32:34 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
-import math
+import matplotlib.pyplot as plt 
 from datetime import datetime
 
 st.set_page_config(page_title="USD/INR Options Suite", layout="wide", page_icon="💹")
